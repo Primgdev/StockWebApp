f3b13e0b64876171ab830d667e677deadc61cb03
Primgdev <primgdev@gmail.com>
2023-12-02 21:52:11 +1100

    update

---
+++ StockWebApp/Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+namespace StockWebApp.Models
+{
+	public class Stock
+	{
+		
+			public int Id { get; set; }
+
+			public string name { get; set; }
+
+			public string quantity { get; set; }
+
+		public Stock()
+		{
+
+		}
+
+		
+	}
+}
+
